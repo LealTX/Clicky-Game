f0c28967edfb430994b7f25e3510f147c4944ef9
LealTX <jacob.leal32@gmail.com>
2019-01-08 20:35:53 -0600

    move over files to github

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import "./header.css";
+
+
+const Header = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">DC Memory Game!</h1>
+    <p className="lead">Click on any heros to earn points. Click on them more than once and you lose!</p>
+  </div>
+</div>
+);
+export default Header;
